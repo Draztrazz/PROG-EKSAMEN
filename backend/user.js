cd5e51b80858f15c456bd901d80fecb79906044f
Draztrazz <mikermjensen@gmail.com>
2020-11-27 10:00:48 +0100

    aa

--- backend/user.js
+++ backend/user.js
@@ -11,6 +11,8 @@ class User{
         this.matched = matched;
     }}
 
-var peter = new User("Peter", "Dreyer", "Male", "Dreyer123", "elskerminhunde123", "Dreyer@gmail.com", [christian, oscar], [], [])
-var christian = new User("Christian", "Bredgaard", "Male", "Chrille321", "gnarlover666", "Chrille@gmail.com", [], [peter], [])
-var oscar = new User("Oscar", "Pedersen", "Male", "Oscarraw", "oskay123", "Peder@hotmail.com", [peter], [christian], [])
+var peter = new User("Peter", "Dreyer", "Male", "Dreyer123", "elskerminhunde123", "Dreyer@gmail.com", [christian, oscar, laura], [], [oscar, laura])
+var christian = new User("Christian", "Bredgaard", "Male", "Chrille321", "gnarlover666", "Chrille@gmail.com", [emilie], [peter], [emilie])
+var oscar = new User("Oscar", "Pedersen", "Male", "Oscarraw", "oskay123", "Peder@hotmail.com", [peter], [christian], [peter])
+var emilie = new User("Emilie", "Lindgren", "Female", "Emilie123", "321eilimE", "Emilie@gmail.com", [christian], [], [christian])
+var laura = new User("Laura", "Laurasen", "Female", "Lauramus123", "Fido123", "Laurahund123@gmail.com", [peter], [], [laura])
