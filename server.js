f945ad540a017aab15f6aaf2b4b7420d1198a7aa
Draztrazz <mikermjensen@gmail.com>
2020-12-09 13:58:53 +0100

    a

--- server.js
+++ server.js
@@ -1,67 +1,113 @@
-if(process.env.NODE_ENV !== "production"){
-    require ("dotenv").config
-}
-
-const express = require("express");
-const app = express();
-const bcrypt = require("bcrypt");
-const passport = require("passport");
-const flash = require("express-flash");
-const session = require("express-session");
-
-const initializePassport = require("./passport-config")
-initializePassport(
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
+  
+  const express = require('express')
+  const app = express()
+  const bcrypt = require('bcrypt')
+  const passport = require('passport')
+  const flash = require('express-flash')
+  const session = require('express-session')
+  const methodOverride = require('method-override')
+  
+  const initializePassport = require('./passport-config')
+  initializePassport(
     passport,
-    email => {
-    return users.find(user => user.email === email)
-})
-
-const users = []
-
-app.set("view-engine", "ejs")
-app.use(express.urlencoded({extended: false}))
-app.use(flash())
-app.use(session({
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+  )
+  
+  const users = []
+  
+  app.set('view-engine', 'ejs')
+  app.use(express.urlencoded({ extended: false }))
+  app.use(flash())
+  app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
-}))
-app.use(passport.initialize())
-pp.use(passport.session())
-
-app.get('/', (req, res) => {
-    res.render("index.ejs")
-})
-
-app.get('/login', (req, res) =>{
-    res.render("login.ejs")
-})
-
-app.post('/login', passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  app.use(methodOverride('_method'))
+  
+  app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', {
+      fname: req.user.fname,
+      lname: req.user.lname,
+      age: req.user.age,
+      gender: req.user.gender})
+  })
+  
+  app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+  })
+  
+  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
     failureFlash: true
-}))
+  }))
 
-app.get('/register', (req, res) =>{
-    res.render("register.ejs")
-})
-
-app.post('/register', async (req, res) => {
+  app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+  })
+  
+  app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+  })
+  
+  app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
-        const hashedPassword = bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            fname: req.body.fname,
-            lname: req.body.lname,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        res.redirect('/login')
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        fname: req.body.fname,
+        lname: req.body.lname,
+        age: req.body.age,
+        gender: req.body.gender,
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect('/login')
     } catch {
-        res.redirect('/register')
+      res.redirect('/register')
     }
-})
+  })
+  app.get('/matches', checkAuthenticated, (req, res) =>{
+    res.render('matches.ejs')
+  })
+  app.get('/emilie', checkAuthenticated, (req, res) => {
+    res.render('emilie.ejs')
+  })
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+  app.get('/jens', checkAuthenticated, (req, res) => {
+    res.render('jens.ejs')
+  })
+
+  app.get('/lone', checkAuthenticated, (req, res) => {
+    res.render('lone.ejs')
+  })
+
+  app.get('/jesper', checkAuthenticated, (req, res) => {
+    res.render('jesper.ejs')
+  })
+  
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/login')
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+  }
+  
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => console.log(`Listening on port ${port}...`));
